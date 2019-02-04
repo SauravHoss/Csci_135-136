8c9abd3a3424f0f0144a7b6cfd7b61fcb17366ce
PleaseEndMyFuckingLife <32362481+SauravHoss@users.noreply.github.com>
2019-02-04 15:34:25 -0500

    Update scrap.cpp

--- scrap.cpp
+++ scrap.cpp
@@ -1,1 +1,19 @@
+Lab_2A
+#include <iostream>
+using namespace std;
 
+int main() 
+{
+  int x;
+  cout << "Enter a number between 0 and 100\n";
+  cin >> x;
+
+  while(x <= 0 || x >= 100)
+  {
+    cout << "Enter a number between 0 and 100\n";
+    cin >> x;
+  }
+    cout << "The number squared is " << (x*x); 
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
