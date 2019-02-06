76fdd561a036f735966f956dc77f9c783f9e5a11
PleaseEndMyFuckingLife <32362481+SauravHoss@users.noreply.github.com>
2019-02-05 22:36:33 -0500

    Update scrap.cpp

--- scrap.cpp
+++ scrap.cpp
@@ -1,86 +1,31 @@
-Lab_2A
+/*
+Author: Saurav Hossain
+Assignment: Project 1B
+Function: Write a program that reads from the cin a sequence of one or more non-negative integers written to be added or subtracted. Space characters can be anywhere in the input. After the input ends (end-of-file is reached), the program should compute and print the result of the input summation.
+Date: 02/03/19
+Instructor: Panda
+File submission name: calc.cpp
+*/
 #include <iostream>
 using namespace std;
-
-int main() 
-{
-  int x;
-  cout << "Enter a number between 0 and 100\n";
-  cin >> x;
-
-  while(x <= 0 || x >= 100)
-  {
-    cout << "Enter a number between 0 and 100\n";
-    cin >> x;
-  }
-    cout << "The number squared is " << (x*x); 
-}
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-Lab_2B
-#include <iostream>
-using namespace std;
-
-int main() 
-{
-  int x,y;
-  cin >> x;
-  cin >> y;
-
-  for(int i = x; i < y; i++)
-  {
-    cout << i << " ";
-  }
-}
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-Lab_2C
-#include <iostream>
-using namespace std;
-
 int main ()
 {
-  int myData[10] = {1};
-  for (int n = 0; n < 10; n++)
-    {
-      myData[n] = 1;
+  int sum;
+  cin >> sum;  // Read the first value as the sum.
+  char opr;         // Operator character
+  int  value;       // New value or term.
+  while (cin >> opr >> value)
+  {    
+    if(opr == '+')
+    { 
+      sum += value;
+      break;
     }
-
-  int x,y;
-  do
-  {
-    //prints array
-    for (int n = 0; n < 10; n++)
+    else
     {
-      cout << myData[n];
+      sum -= value;
+      break;
     }
-    cout << "\n";
-    cin >> x;
-    cin >> y;
-
-    myData[x] = y;
-
   }
-  while(x >= 0 && x < 10);
-  cout << "Exit.";
-  
-}
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-Lab_2D
-#include <iostream>
-using namespace std;
-
-int main ()
-{
-  int fib[60] = {0,1};  
-
-  for(int i = 2; i < 60 ; i++)
-  {
-    fib[i] = fib[i-2] + fib[i-1];
-  }
-
-  for(int i = 0; i < 60; i++)
-  {
-    cout << fib[i] << "\n";
-  }
+  cout << sum;
 }
