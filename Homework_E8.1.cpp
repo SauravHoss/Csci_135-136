a5c2512767d5bada7f10e3a784353c32ccd74a07
PleaseEndMyFuckingLife <32362481+SauravHoss@users.noreply.github.com>
2019-02-13 15:57:38 -0500

    Add files via upload

---
+++ Homework_E8.1.cpp
@@ -0,0 +1,37 @@
+/*
+Author: Saurav Hossain
+Date: 01/30/19
+Prompt: Write a program that carries out the following tasks:
+      Open a file with the name hello.txt.
+      Store the message “Hello, World!” in the file.
+      Close the file.
+      Open the same file again.
+      Read the message into a string variable and print it.
+*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main ()
+{
+	ofstream outFile;
+	outFile.open("hello.txt");
+
+	outFile << "Hello, World!" << endl;
+
+	outFile.close();
+
+	ifstream inFile; 
+	inFile.open("hello.txt");
+	
+	string s;	
+	
+	while(!inFile.eof())
+	{
+		inFile >> s;
+		cout << s;
+	}
+}
