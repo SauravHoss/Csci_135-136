6788ad3c40e247b468b779d1b25a73cf4635c56a
PleaseEndMyFuckingLife <32362481+SauravHoss@users.noreply.github.com>
2019-03-06 17:53:38 -0500

    Update scrap.cpp

--- scrap.cpp
+++ scrap.cpp
@@ -16,3 +16,33 @@ int main()
 }
 
 // lab 6b
+
+
+//loops upper and lower case
+char shiftChar(char c, int rshift)
+{
+  char r;
+  if((int)c <= 122 && (int)c >= 97 )
+  {
+    if(((int)c + rshift) > 122)
+    {
+      int x = ((int)c + rshift) - 122;
+      r = 96 + x;
+    } 
+    else
+      r = (int)c + rshift;
+  }
+  else if((int)c <= 90 && (int)c >= 65 )
+  {
+    if(((int)c + rshift) > 90)
+    {
+      int x = ((int)c + rshift) - 90;
+      r = 64 + x;
+    } 
+    else
+      r = (int)c + rshift;
+  }
+  return r;
+}
+
+
