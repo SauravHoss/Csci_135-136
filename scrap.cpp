b2531ae5efa6fe098efa524f375a8556b388f686
PleaseEndMyFuckingLife <32362481+SauravHoss@users.noreply.github.com>
2019-03-06 18:02:21 -0500

    Update scrap.cpp

--- scrap.cpp
+++ scrap.cpp
@@ -16,6 +16,61 @@ int main()
 }
 
 // lab 6b
+#include <iostream>
+#include <string>
+using namespace std;
+
+char shiftChar(char c, int rshift)
+{
+  char r;
+  if(isalpha(c))
+  {
+    if((int)c <= 122 && (int)c >= 97 )
+    {
+      if(((int)c + rshift) > 122)
+      {
+        int x = ((int)c + rshift) - 122;
+        r = 96 + x;
+      } 
+      else
+        r = (int)c + rshift;
+    }
+    else if((int)c <= 90 && (int)c >= 65 )
+    {
+      if(((int)c + rshift) > 90)
+      {
+        int x = ((int)c + rshift) - 90;
+        r = 64 + x;
+      } 
+      else
+        r = (int)c + rshift;
+    }
+  }
+  else
+  {
+    r = c;
+  }
+  return r;
+}
+
+string encryptCaesar(string plaintext, int rshift)
+{
+  string s = "";
+  for(int i = 0; i < plaintext.length(); i++)
+  {
+    char c = plaintext[i];
+    s = s + shiftChar(c, rshift);
+  }
+  return s;
+}
+
+int main() 
+{
+//  cout << shiftChar('v', 7);
+  cout << encryptCaesar("Hello, World!", 10);
+}
+
+//lab 6c
 
 
 //loops upper and lower case
