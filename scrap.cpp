778387e607520d9dbe642768a3f16e94844a042a
PleaseEndMyFuckingLife <32362481+SauravHoss@users.noreply.github.com>
2019-04-17 09:43:03 -0400

    Update scrap.cpp

--- scrap.cpp
+++ scrap.cpp
@@ -1,151 +1,54 @@
-/*
-Author: Saurav Hossain
-Assignment: Lab 10E
-Function: Write a function that should return true if the two time slots overlap, otherwise return false. (Take into account the starting times of the time slots and the duration of the scheduled movies.)
-Date: 04/11/19
-Instructor: Panda
-File submission name: time.cpp
-*/
-
 #include <iostream>
-#include <cmath>
 using namespace std;
 
-//variable for types of movie
-enum Genre {ACTION, COMEDY, DRAMA, ROMANCE, THRILLER};
-
-//variable for types of daytime
-enum Showtimes {EARLYBIRD, MORNING, AFTERNOON, EVENING, LATENIGHT};
-
-//class time
-class Time 
+class Circuit
 {
   public:
-    int h;
-    int m;
+    int first_switch;
+    int second_switch;
+    int lamp;
+
+    int get_first_switch_state(); // 0 for down, 1 for up
+    int get_second_switch_state();
+    int get_lamp_state(); // 0 for off, 1 for on
+    void toggle_first_switch();
+    void toggle_second_switch();
 };
 
-//class movie
-class Movie 
-{ 
-	public: 
-    	string title;
-    	Genre genre;     // only one genre per movie
-    	int duration;    // in minutes
-};
-
-//class TimeSlot
-class TimeSlot 
-{ 
-	public: 
-    	Movie movie;     // what movie
-    	Time startTime;  // when it starts
-};
-
-//prints time
-void printTime(Time time) 
+// 0 for down, 1 for up
+int Circuit:: get_first_switch_state()
 {
-  cout << time.h << ":" << time.m;
-}
-
-//minutes since midnight
-int minutesSinceMidnight(Time time)
-{
-  int x = ((time.h * 60) + time.m);
-  if(x >= 1440)
-    return abs(1440 - x);
-  else
-    return x;
-}
-
-//minutes until time a becomes time b
-int minutesUntil(Time earlier, Time later)
-{
-  return minutesSinceMidnight(later) - minutesSinceMidnight(earlier);
-}
-
-//prints movie
-void printMovie(Movie mv)
-{
-    string g;
-    switch (mv.genre) 
-    {
-        case ACTION   : g = "ACTION"; break;
-        case COMEDY   : g = "COMEDY"; break;
-        case DRAMA    : g = "DRAMA";  break;
-        case ROMANCE  : g = "ROMANCE"; break;
-        case THRILLER : g = "THRILLER"; break;
-    }
-    cout << mv.title << " " << g << " (" << mv.duration << " min)";
-}
-
-//converts minutes to time
-Time minToTime(int min)
-{
-  int mins = min % 60;
-  int hours =  min / 60;
-
-  Time t = {hours, mins};
-  return t;
-}
-
-//adds minutes to given time and returns new time
-Time addMinutes(Time time0, int min)
-{
-  return minToTime(((time0.h * 60) + time0.m) + min);
-}
-
-//prints timeslot
-void printTimeSlot(TimeSlot ts)
-{
-	printMovie(ts.movie);
-  cout << " [starts at "; 
-	printTime(ts.startTime);
-  cout << ", ends by ";
-
-  Time endTime;
-  endTime = addMinutes(ts.startTime, ts.movie.duration);
-
-  printTime(endTime); 
-  cout << "]";
-}
-
-//returns timeslot of new movie
-TimeSlot scheduleAfter(TimeSlot ts, Movie nextMovie)
-{
-  TimeSlot nextts;
-  nextts.movie = nextMovie;
-  nextts.startTime = addMinutes(ts.startTime, ts.movie.duration);
-  return nextts;
-}
-
-//shows if times overlap
-bool timeOverlap(TimeSlot ts1, TimeSlot ts2)
-{ 
-  if(abs(minutesUntil(ts1.startTime, ts2.startTime)) >= ts1.movie.duration) 
-    return false;
-  else
-    return true;
+  return first_switch;
 } 
 
-//main method
+int Circuit:: get_second_switch_state()
+{
+  return second_switch;
+}
+
+// 0 for off, 1 for on
+int Circuit:: get_lamp_state()
+{
+  return lamp;
+} 
+
+void Circuit:: toggle_first_switch()
+{
+  if(first_switch == 1)
+    first_switch = 0;
+  else 
+    first_switch = 1;
+}
+
+void Circuit:: toggle_second_switch()
+{
+  if(second_switch == 1)
+    second_switch = 0;
+  else
+    second_switch = 1;
+}
+
 int main() 
 {
-  Movie m;
-  m.title = "Sugma";
-  m.genre = COMEDY;
-  m.duration = 65;
-
-  Time time1 = {10, 30};
-  Time time2 = {8, 30};
-
-  TimeSlot t1;
-  t1.movie = m;
-  t1.startTime = time1;
-
-  TimeSlot t2;
-  t2.movie = m;
-  t2.startTime = time2;
-
-  cout << timeOverlap(t1, t2);
+  
 }
